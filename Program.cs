a3da5dd835903cecf3ddb0a50c35268f8abdeb40
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-10 14:15:16 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int SumBetween(int m, int n)
+{   
+    int sum = 0;
+    sum += m;
+    ++m;
+    if (m <= n)
+        sum += SumBetween(m, n);
+    return sum;
+}
+
+Console.WriteLine("Введите значения начала и конца промежутка");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Сумма чисел в промежутке [{0};{1}] равна : {2} ", m, n, SumBetween(m, n));
+Console.WriteLine(SumBetween(m, n));
